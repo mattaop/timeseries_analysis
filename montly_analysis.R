9c078b5168e9aa9308bf6ee945602d1ec61636e2
Joakim Olsen <joakiol@stud.ntnu.no>
2019-09-16 15:08:01 +0200

    Differencing, acf/pacf and some arima

--- montly_analysis.R
+++ montly_analysis.R
@@ -1,3 +1,5 @@
+library(forecast)
+
 monthly.df = read.csv('data/monthly_in_situ_co2_mlo.csv', skip=58, sep=",", header=FALSE)
 monthly.ts = ts(data=monthly.df$V5[2:737], frequency = 12, start=c(1958,3), end = c(2019,6))
 
@@ -6,6 +8,24 @@ for (i in 1:736) {
 }
 
 
-monthly.dc = decompose(monthly.ts)
+monthly.dc = decompose(monthly.ts, type = "multiplicative")
 
 plot(monthly.dc)
+
+acf(monthly.dc$random, na.action = na.pass, lag.max = 36)
+
+diff1 = diff(monthly.ts, lag=1)
+diff1 = diff(diff1, lag=12)
+
+plot(diff1)
+
+acf(diff1, na.action = na.pass, lag.max = 36)
+pacf(diff1, na.action = na.pass, lag.max = 36)
+
+model = arima(diff1, order = c(0,0,1), seasonal = c(0,0,1), include.mean = FALSE)
+summary(model)
+model$aicc
+
+
+auto.arima(diff1, d = 0, D = 0)
+auto.arima(diff1, d = 0, D = 0, max.p=0, max.q=1, max.P=0, max.Q=1, ic = "aicc")
