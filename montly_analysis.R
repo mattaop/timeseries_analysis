d48ea39576116f88fb4ae4ad0df4e33085c2d815
mattaop <snowboard95@live.no>
2019-09-20 13:22:30 +0200

    Bootstrap

--- montly_analysis.R
+++ montly_analysis.R
@@ -40,34 +40,37 @@ pacf(model$residuals)
 # plot.forecast(futurVal)
 # Fit auto-arima to find "best" model, with and wothout restrains
 # auto.arima(diff1, d = 0, D = 0, ic="aicc")
-
 print(model$coef)
+print(model$vcov)
 observed_residuals <- model$residuals
-simulate_arima_values <- function(x_0, parameters, observed_residuals){
-  x <- vector(length = length(observed_residuals)+1)
-  x[1] <- x_0
-  for(i in 1:length(observed_residuals)){
-    vector[i+1] <- parameters
-    
+
+simulate_arima_values <- function(parameters, residuals){
+  for(i in 14:(736+13)){
+    index <- sample(14:736, size=1)$value # Sample T random residuals from 
+    print(index)
+    z[i-13] <- (residuals[index]
+               +parameters[1]*residuals[index-1]
+               +parameters[2]*residuals[index-12]
+               +parameters[1]*parameters[2]*residuals[index-13])
   }
+  return(z)
 }
 simulate_sequence <- function(data, model, observed_residuals){
-  # residual_samples <- sample(observed_residuals, size=736, replace=TRUE) # Sample T random residuals from 
-
   # index_1 <- sample(1:736, 1) # Get index for a random sample from the data, but not the last
   # x_1 <- data[index_1] # Get a random x from the data
   # x_2 <- data[index_1+1] # Get the following x
-  simulated_data <- simulate(model, nsim=736)
-  simulated_model <- arima(simulated_data, order = c(0,0,1), seasonal = c(0,0,1), include.mean = TRUE) # Fit betas for LS to the sequence
+  # simulated_data <- simulate(model, nsim=736)
+  # simulated_model <- arima(simulated_data, order = c(0,0,1), seasonal = c(0,0,1), include.mean = FALSE) # Fit betas for LS to the sequence
+  simulated_data  <- simulate_arima_values(model$coef, residuals)
+  simulated_model <- arima(ts(data=simulated_data, frequency = 12), order = c(0,0,1), seasonal = c(0,0,1), include.mean = FALSE) # Fit betas for LS to the sequence
   beta <- simulated_model$coef
   return (beta)
 }
 beta <- simulate_sequence(diff1, model, observed_residuals)
 print(beta)
 
-
 sample_bootstrap <- function(B, diff1, model, observed_residuals){
-  sampled_beta <- matrix(nrow=3, ncol=B) # Matrix to store sampled beta
+  sampled_beta <- matrix(nrow=2, ncol=B) # Matrix to store sampled beta
   for (i in 1:B){
     # Return betas for random generated sequences
     beta_hat <- simulate_sequence(diff1, model, observed_residuals) 
@@ -75,13 +78,13 @@ sample_bootstrap <- function(B, diff1, model, observed_residuals){
   }
   observed_mean <- rowSums(sampled_beta)/B # Compute observed mean for sampled betas for LS
   observed_bias <- model$coef - observed_mean # Compute observed bias for sampled betas for LA
-  observed_variance <- c(var(sampled_beta[1,]), var(sampled_beta[2,]), var(sampled_beta[3,])) # Compute 
-  # observed variance for sampled betas for LS
+  observed_variance <- c(var(sampled_beta[1,]), var(sampled_beta[2,])) # Compute 
+  # observed variance for sampled betas
   return(list(beta = sampled_beta, mean = observed_mean, bias = observed_bias, variance = observed_variance))
 }
-B = 1000
+B = 10000
 bootstrap_samples = sample_bootstrap(B, diff1, model, observed_residuals)
-print(bootstrap_samples$beta)
+# print(bootstrap_samples$beta)
 print(bootstrap_samples$mean)
 print(bootstrap_samples$bias)
 print(bootstrap_samples$variance)
@@ -91,6 +94,3 @@ abline(v=c(model$coef[1], bootstrap_samples$mean[1], quantile(bootstrap_samples$
 hist(bootstrap_samples$beta[2,], freq = F, breaks=40, main="Histogram of bootstrap samples", 
      xlab="Sampled beta_2 values", ylab="Denisty", sub = "Figure 2: Histogram of bootstrap samples of sma1 parameter.")
 abline(v=c(model$coef[2], bootstrap_samples$mean[2], quantile(bootstrap_samples$beta[2,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
-hist(bootstrap_samples$beta[3,], freq = F, breaks=40, main="Histogram of bootstrap samples", 
-     xlab="Sampled beta_2 values", ylab="Denisty", sub = "Figure 2: Histogram of bootstrap samples of intercept.")
-abline(v=c(model$coef[3], bootstrap_samples$mean[3], quantile(bootstrap_samples$beta[3,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
