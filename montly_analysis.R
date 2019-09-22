b9e164e4aaacb3709cc6358e9b3ca99bff979db3
mattaop <snowboard95@live.no>
2019-09-22 20:44:14 +0200

    problemet med Nan er kanskje borte

--- montly_analysis.R
+++ montly_analysis.R
@@ -70,6 +70,18 @@ bootstrap <- function(data, model){
   x <- vector(length=736+2)
   x[1] <- data[bootstrap_index] # Get a random x from the data
   x[2] <- data[bootstrap_index+1]
+  if (is.nan(x[1])){
+    print(length(data))
+    print(bootstrap_index)
+    print(x[1])
+    print(x[2])
+  }
+  if(is.nan(x[2])){
+    print(length(data))
+    print(bootstrap_index)
+    print(x[1])
+    print(x[2])
+  }
   for(i in 1:736){
     j <- i + 2 # Shift timeseries to be indexed from -1 to 736
     k <- i + 13 # Shift residual array to be indexed from -12 to 736
@@ -81,49 +93,43 @@ bootstrap <- function(data, model){
              +model$coef['ma1']*model$coef['sma1']*bootstrap_residuals[k-13]
              )
   }
-  generated_timeseries <- ts(data=tail(x, -2), frequency = 12, start=c(1958,3), end = c(2019,6))
+  generated_timeseries <- ts(data=tail(x, -2), frequency = 12)
   return(generated_timeseries)
 }
-simulate_sequence <- function(data, model){
-  #simulated_data <- simulate(model, nsim=736)
-  simulated_data  <- bootstrap(data, model)
-  simulated_model <- arima(simulated_data, order = c(2,0,1), seasonal = c(0,0,1), include.mean = FALSE)
-  beta <- simulated_model$coef
-  return (beta)
-}
-beta <- simulate_sequence(model1, model)
-
 sample_parameters <- function(B, diff1, model){
   sampled_beta <- matrix(nrow=4, ncol=B) # Matrix to store sampled beta
   for (i in 1:B){
-    # Return betas for random generated sequences
-    beta_hat <- simulate_sequence(diff1, model) 
+    #simulated_data <- simulate(diff1, nsim=736) # Simulate time series data
+    simulated_data  <- bootstrap(diff1, model) # Bootstrap time series data
+    simulated_model <- arima(simulated_data, order = c(2,0,1), seasonal = c(0,0,1), include.mean = FALSE)
+    beta_hat <- simulated_model$coef 
     sampled_beta[,i] <- beta_hat # Store betas
   }
   observed_mean <- rowSums(sampled_beta)/B # Compute observed mean for sampled betas for LS
   observed_bias <- model$coef - observed_mean # Compute observed bias for sampled betas for LA
   observed_variance <- c(var(sampled_beta[1,]), var(sampled_beta[2,]),
-                         var(sampled_beta[3,])
-                         ,var(sampled_beta[4,])
-                         ) # Compute 
-  # observed variance for sampled betas
+                         var(sampled_beta[3,]),var(sampled_beta[4,])
+                         ) # Compute observed variance for sampled betas
   return(list(beta = sampled_beta, mean = observed_mean, bias = observed_bias, variance = observed_variance))
 }
-B = 1000
-bootstrap_samples = sample_parameters(B, model1, model)
-# print(bootstrap_samples$beta)
-print(bootstrap_samples$mean)
-print(bootstrap_samples$bias)
-print(bootstrap_samples$variance)
-hist(bootstrap_samples$beta[1,], freq = F, breaks=40, main="Histogram of simulated samples", 
+B = 10000
+simulated_samples = sample_parameters(B, model1, model)
+
+# Print statistics
+print(simulated_samples$mean)
+print(simulated_samples$bias)
+print(simulated_samples$variance)
+
+# Plot sampled parameters
+hist(simulated_samples$beta[1,], freq = F, breaks=40, main="Histogram of simulated samples", 
      xlab="Ar1", ylab="Denisty")
-abline(v=c(model$coef[1], bootstrap_samples$mean[1], quantile(bootstrap_samples$beta[1,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
-hist(bootstrap_samples$beta[2,], freq = F, breaks=40, main="Histogram of simulated samples", 
+abline(v=c(model$coef[1], simulated_samples$mean[1], quantile(simulated_samples$beta[1,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
+hist(simulated_samples$beta[2,], freq = F, breaks=40, main="Histogram of simulated samples", 
      xlab="Ar2", ylab="Denisty")
-abline(v=c(model$coef[2], bootstrap_samples$mean[2], quantile(bootstrap_samples$beta[2,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
-hist(bootstrap_samples$beta[3,], freq = F, breaks=40, main="Histogram of simulated samples", 
+abline(v=c(model$coef[2], simulated_samples$mean[2], quantile(simulated_samples$beta[2,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
+hist(simulated_samples$beta[3,], freq = F, breaks=40, main="Histogram of simulated samples", 
      xlab="Ma1", ylab="Denisty")
-abline(v=c(model$coef[3], bootstrap_samples$mean[3], quantile(bootstrap_samples$beta[3,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
-hist(bootstrap_samples$beta[4,], freq = F, breaks=40, main="Histogram of simulated samples", 
+abline(v=c(model$coef[3], simulated_samples$mean[3], quantile(simulated_samples$beta[3,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
+hist(simulated_samples$beta[4,], freq = F, breaks=40, main="Histogram of simulated samples", 
      xlab="Sma1", ylab="Denisty")
-abline(v=c(model$coef[4], bootstrap_samples$mean[4], quantile(bootstrap_samples$beta[4,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
+abline(v=c(model$coef[4], simulated_samples$mean[4], quantile(simulated_samples$beta[4,], c(0.025, 0.975))), col=c("red", "yellow", "blue", "blue"))
